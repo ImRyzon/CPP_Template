6b9e6eecec5937c8a2131175bd42999d61da3e9e
ImRyzon <70490197+ImRyzon@users.noreply.github.com>
2022-06-08 18:51:40 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MM = INT_MAX;
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int testcases = 1;
+
+    for (int i = 0; i < testcases; i++) {
+        solve();
+    }
+}
